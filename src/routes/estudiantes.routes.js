5e2b64e0f63a5f114584ab78921f9dabeaaf33af
Gabriel Uribe Gomez <info@colsysnet.com>
2024-02-24 20:11:25 -0500

    first commit

---
+++ src/routes/estudiantes.routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { consultarEstudiantes } from '../controllers/estudiantes.controller.js'
+
+
+const router = Router()
+
+router.get('/estudiantes', consultarEstudiantes);
+
+export default router
